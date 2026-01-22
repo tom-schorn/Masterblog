abee6db2b4e8fc66f8ea4094c94a2289de08fa20
tom-schorn <t.schorn@ts-mail.de>
2026-01-22 16:45:00 +0100

    System for adding blog posts

--- app.py
+++ app.py
@@ -1,11 +1,50 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    # Fetch the blog posts from the JSON file
+    with open('data.json', 'r') as file:
+        blog_posts = json.load(file)
+    return render_template('index.html', posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        # Get the data from the form
+        author = request.form.get('author')
+        title = request.form.get('title')
+        content = request.form.get('content')
+
+        # Load existing posts
+        with open('data.json', 'r') as file:
+            blog_posts = json.load(file)
+
+        # Generate a unique ID
+        new_id = max(post['id'] for post in blog_posts) + 1
+
+        # Create new post
+        new_post = {
+            'id': new_id,
+            'author': author,
+            'title': title,
+            'content': content
+        }
+
+        # Add the new post to the list
+        blog_posts.append(new_post)
+
+        # Save back to the JSON file
+        with open('data.json', 'w') as file:
+            json.dump(blog_posts, file, indent=4)
+
+        return redirect(url_for('index'))
+
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
